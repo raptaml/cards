8c9689ac2418c7d11c8fadc474158e0b703ea4d5
Matthias Lemmer <lemmer@kks.local>
2016-12-30 15:04:20 +0100

    implement deal

--- lib/cards.ex
+++ lib/cards.ex
@@ -6,13 +6,17 @@ defmodule Cards do
         for suit <- suits, value <- values do
                 "#{value} of #{suit}"
         end
-        
-
     end
+
     def shuffle(deck) do
         Enum.shuffle(deck)
     end
+
     def contains?(deck, card) do
         Enum.member?(deck, card)
+    end
+
+    def deal(deck, hand_size) do
+        Enum.split(deck, hand_size)
     end
 end
