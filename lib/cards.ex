a7343063f63bb7d22bbaaf9afc76f5786beccd6b
matze <matthiaslemmer@aol.de>
2016-12-31 17:07:21 +0100

    implement load and deal_first_hand plus docs

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,4 +1,8 @@
 defmodule Cards do
+    @moduledoc"""
+        Provides methods for creating and handling a deck of cards
+    """
+
     def create_deck do
         values = ["Ace", "Two", "Three", "For", "Five"]
         suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
@@ -23,5 +27,16 @@ defmodule Cards do
     def save(deck, path) do
         binary = :erlang.term_to_binary(deck)
         File.write(path, binary)
+    end
+
+    def load(path) do
+        case File.read(path) do
+            {:ok, binary} -> :erlang.binary_to_term(binary)
+            {:error, _reason} -> "This file does not even exist in your wildest dreams..."
+        end    
+    end
+
+    def create_first_hand(hand_size) do
+        create_deck |> shuffle |> deal(hand_size)
     end
 end
